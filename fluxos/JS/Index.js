d4c8c6635cebcb51f3859cebc3ce93d2c7829747
Lucas <lucasno@al.insper.edu.br>
2022-06-15 01:03:57 -0300

    att

--- fluxos/JS/Index.js
+++ fluxos/JS/Index.js
@@ -1,30 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    let tecla = document.querySelector('.botao')
-    let quadro = document.querySelector('.escrita')
-
-    function um(){
-        
-        a = document.createElement('p')
-        a.innerHTML = 'O campo de texto está vazio'
-        quadro.appendChild(a)
-        a.style.color = 'red'
-        a.style.marginLeft = '1.4rem'
-        a.style.marginBottom = '1rem'
-    }
-
-
-
-
-    tecla.addEventListener('click', function(){
-
-        if (input.value == 0){
-            um()
-        }
-        else {
-            
-        }
-    })
 
     localStorage.setItem('tsegunda', 0)
     localStorage.setItem('tterca', 0)
@@ -34,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
     localStorage.setItem('tsabado', 0)
     localStorage.setItem('tdomingo', 0)
     localStorage.setItem('diaatual', 0)
+    localStorage.setItem('novotreino', '')
     
 
 })
--- fluxos/JS/inicio.js
+++ fluxos/JS/inicio.js
@@ -1,21 +1,57 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    let butt = document.querySelector('.case')
+    let butt = document.querySelectorAll('.case')
+
+    butt0 = butt[0]
+    butt1 = butt[1]
+    butt2 = butt[2]
+    butt3 = butt[3]
 
 
-    butt.addEventListener('click', function(){
+    butt0.addEventListener('click', function(){
 
-        horai = butt.querySelector('.horai')
-        horaf = butt.querySelector('.horaf')
-        desc = butt.querySelector('.desc')
+        horai = butt0.querySelector('.horai')
+        horaf = butt0.querySelector('.horaf')
+        desc = butt0.querySelector('.desc')
 
         localStorage.setItem('horai_NT', horai.innerHTML)
         localStorage.setItem('horaf_NT', horaf.innerHTML)
         localStorage.setItem('descricao_NT', desc.innerHTML)
-
-
     })
+    butt1.addEventListener('click', function(){
+
+        horai = butt1.querySelector('.horai')
+        horaf = butt1.querySelector('.horaf')
+        desc = butt1.querySelector('.desc')
+
+        localStorage.setItem('horai_NT', horai.innerHTML)
+        localStorage.setItem('horaf_NT', horaf.innerHTML)
+        localStorage.setItem('descricao_NT', desc.innerHTML)
+    })
+
+    butt2.addEventListener('click', function(){
+
+        horai = butt2.querySelector('.horai')
+        horaf = butt2.querySelector('.horaf')
+        desc = butt2.querySelector('.desc')
+
+        localStorage.setItem('horai_NT', horai.innerHTML)
+        localStorage.setItem('horaf_NT', horaf.innerHTML)
+        localStorage.setItem('descricao_NT', desc.innerHTML)
+    })
+
+    butt0.addEventListener('click', function(){
+
+        horai = butt3.querySelector('.horai')
+        horaf = butt3.querySelector('.horaf')
+        desc = butt3.querySelector('.desc')
+
+        localStorage.setItem('horai_NT', horai.innerHTML)
+        localStorage.setItem('horaf_NT', horaf.innerHTML)
+        localStorage.setItem('descricao_NT', desc.innerHTML)
+    })
+
 
 
 
